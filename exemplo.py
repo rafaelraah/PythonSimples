fe03527dc2f93e27b4a3780063c22cba40878fc1
Rafael <rafael@teste.com.br>
2026-04-30 07:30:30 -0300

    Atualizacao, tratamentos de string Atualizacao tratamentos de strings esc exit

--- exemplo.py
+++ exemplo.py
@@ -1,3 +1,10 @@
 print("Hello World");
 idade=31
 print(idade);
+
+entrada = input("Digite o seu nome: ")
+print ("Seu nome é:", entrada)
+
+print("O nome", entrada, "tem", len(entrada), "caracteres. As três primeiras letras do nome", entrada, "são", entrada[:3], "e as três últimas são", entrada[-3:])
+print("O nome", entrada, "de trás para a frente é", entrada[::-1])
+print("Em caixa alta:", entrada.upper())
