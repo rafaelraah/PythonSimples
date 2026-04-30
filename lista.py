255b103232d4fa99eaef4912e90af74216f02191
Rafael <rafaelsantos.rsdn@gmail.com>
2026-04-30 08:41:29 -0300

    Manipulação de listas em python

--- lista.py
+++ lista.py
@@ -1,3 +1,9 @@
+def StringComTodosOsNomes(nomes):
+    retornaNomes = "";
+    for item in nomes:
+        retornaNomes = retornaNomes + item + " ";
+    return retornaNomes;
+
 numeros = [10, 20, 30]
 nomes = ["Rafael", "Pedro", "Bruno"]
 
@@ -14,3 +20,36 @@ for item in nomes:
     todosNomesStr = todosNomesStr + item + " ";
 
 print("Todos os nomes da lista: " + todosNomesStr)
+
+nomes[2] = "Daniel"
+
+todosNomesStr = StringComTodosOsNomes(nomes);
+print("Todos os nomes da lista: " + todosNomesStr)
+
+nomes[3] = "Fernando"
+todosNomesStr = StringComTodosOsNomes(nomes);
+print("Todos os nomes da lista: " + todosNomesStr)
+
+nomes.pop(1);
+todosNomesStr = StringComTodosOsNomes(nomes);
+print("Todos os nomes da lista: " + todosNomesStr)
+
+nomes.remove("Fernando");
+print("Todos os nomes da lista: " + StringComTodosOsNomes(nomes))
+
+if("Daniel" in nomes):
+    nomes.remove("Daniel")
+   
+if("Amanda" in nomes):
+    nomes.remove("Amanda")
+else:
+    print("O nome Amanda não está na lista, então não será removido");
+   
+print("Todos os nomes da lista: " + StringComTodosOsNomes(nomes))
+
+
+
+
+
+
+
